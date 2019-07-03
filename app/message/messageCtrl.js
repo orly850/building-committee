5eac6e8c5812b68cfa11b2703a67229f65b2acdc
orly <orly850@gmail.com>
2019-07-03 18:52:54 +0300

    font awesome

--- app/message/messageCtrl.js
+++ app/message/messageCtrl.js
@@ -1,33 +1,37 @@
 
-app.controller("messageCtrl", function ($scope,  msgSrv,userSrv) {
+app.controller("messageCtrl", function ($scope, msgSrv, userSrv) {
 
-    msgSrv.getMsg().then(function (msgArr) {
-        $scope.msgArr = msgArr;
-    }, function (err) {
-        $log.error(err);
-    })
+  msgSrv.getMsg().then(function (msgArr) {
+    $scope.msgArr = msgArr;
+  }, function (err) {
+    $log.error(err);
+  })
 
 
 
   //-----------------------------------------------------
-    $scope.active = userSrv.getActive();
+  $scope.active = userSrv.getActive();
   //-----------------------------------------------------
 
-    $scope.val = "";
-    $scope.query = "";
-    $scope.filterMsg = function (Msg) {
-        if (Msg.title.toLowerCase().includes($scope.query.toLowerCase())) {
-            return true;
-        } else {
-            return false;
-        }
+  $scope.val = "";
+  $scope.query = "";
+  $scope.filterMsg = function (Msg) {
+    if (Msg.title.toLowerCase().includes($scope.query.toLowerCase())) {
+      return true;
+    } else {
+      return false;
     }
-      //-----------------------------------------------------
+  }
+  //-----------------------------------------------------
 
-    $scope.isCommittee = function () {
+  $scope.isCommittee = function () {
     return $scope.active.isCommitteeMember
-    
+  }
 
-    }
+  //----------------------------------
+  $scope.isCommittee = function () {
+    return $scope.active.isCommitteeMember
+  }
+
 
 })
