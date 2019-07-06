fda87b98bd1f2cf7d25e2ce6fa700b8a84fa5ede
orly <orly850@gmail.com>
2019-07-06 14:55:44 +0300

    new vote modal - form

--- app/voting/newVoteCtrl.js
+++ app/voting/newVoteCtrl.js
@@ -1,16 +1,43 @@
 
-app.controller("newVoteCtrl", function ($scope,  userSrv, voteSrv) {
+app.controller("newVoteCtrl", function ($scope, userSrv, voteSrv) {
 
 
     $scope.user = userSrv.getActive();
 
-    $scope.title ;
-    $scope.details ;
-    $scope.priorty ;
+    $scope.title;
+    $scope.details;
+    $scope.options;
     $scope.name = $scope.user.name;
 
     $scope.newVote = function () {
-        voteSrv.newVote( $scope.name,  $scope.title, $scope.details, $scope.priorty)
+        voteSrv.newVote($scope.name, $scope.title, $scope.details, $scope.options)
     };
+    //--------------------------------------------
+
+
+    $scope.arr = [];
+    // $scope.count = 0;
+
+    $scope.add = function () {
+        if (!$scope.options) {
+            alert("Enter Option")
+        } else {
+            for (i = 0; i < $scope.arr.length; i++) {
+                if ($scope.options === $scope.arr[i]) {
+                    alert("Duplicate! Try again");
+                    return;
+                }
+            }
+            $scope.arr.push($scope.options);
+            // $scope.count++;
+            $scope.options = "";
+
+        }
+
+    }
+
+
+
+
 
 })
--- app/voting/votingCtrl.js
+++ app/voting/votingCtrl.js
@@ -11,7 +11,7 @@ app.controller("votingCtrl", function ($scope, voteSrv, userSrv) {
     
       //----------------------------------
     
-
+    
     voteSrv.getVots().then(function (arr) {
         $scope.votesArr = arr;
       }, function (err) {
