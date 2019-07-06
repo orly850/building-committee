4fcf740e6ca4a970f239d9bcdd3185acc77acd61
orly <orly850@gmail.com>
2019-07-06 23:12:55 +0300

    bkup

--- app/tenants/tenantsCtrl.js
+++ app/tenants/tenantsCtrl.js
@@ -17,6 +17,23 @@ app.controller("tenantsCtrl", function ($scope, userSrv) {
         userSrv.deleteUser(user);
     
     }
+//--------------------- filter
+// $scope.val 
+// $scope.val2 = "";
+
+$scope.query = "";
+$scope.filterTenant = function (tenent) {
+  if (tenent.name.toLowerCase().includes($scope.query.toLowerCase())) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// this.name = name;
+// this.email = email;
+// this.appartment = appartment;
+
 
 })
 
